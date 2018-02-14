11f877edb44dc29d773b78432bc36dbafc3a441c
David Morales <morgar.creative@gmail.com>
2018-02-14 12:48:21 -0600

    setup database and handlebars

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var mySQL =  require('mysql');
+var connection = mySQL.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log('connected as id' + connection.threadId);
+});
+
+module.exports = connection;
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var burger = require("../models/burger");
 
-router.get('/', function(req, res){
-    res.render('index');
-})
+router.get("/", function(req, res) {
+  burger.all(function(burger_data) {
+    console.log(burger_data);
+    res.render("index", { burger_data });
+  });
+});
+
+router.put("/burgers/update", function(req, res) {
+  burger.update(req.body.burger_id, function(result) {
+    console.log(result);
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
